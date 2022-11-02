573fbd0f1be2dfcf3210831c6f06a67cfc914584
long1703@outlook.com <long1703@outlook.com>
2022-11-02 09:04:10 +0700

    insert data and view data

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,22 +1,31 @@
 const Course = require("../model/Course");
-
+const {mutipleMongooseToObject} = require("../../ulti/mongoose")
 class SiteController {
 
 
     // [get]  /
-    index(req, res) {
+    // index(req, res) {
 
-        // res.render('home');
-        Course.find({}, function (err, course) {
-            if (!err) {
-                res.json(course);
+    //     // res.render('home');
+    //     Course.find({}, function (err, course) {
+    //         if (!err) {
+    //             res.json(course);
 
-            }
-            else {
+    //         }
+    //         else {
 
-                res.status(404).json({ error: "errorr!!" });
-            }
-        })
+    //             res.status(404).json({ error: "errorr!!" });
+    //         }
+    //     })
+    // }
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {  
+                res.render('home', {
+                    courses : mutipleMongooseToObject(courses)
+                })
+            })
+            .catch(err => next(err))
     }
     // [get] /search/
     // định nghĩa param tuyến đường
