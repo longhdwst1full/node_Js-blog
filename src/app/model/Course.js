b09fc17daafbdc84e7b4aa71ee2f989eecb2c124
long1703@outlook.com <long1703@outlook.com>
2023-03-07 09:34:09 +0700

    update

--- src/app/model/Course.js
+++ src/app/model/Course.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
-
+const AutoIncrement = require('mongoose-sequence');
 
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
+    _id: { type: Number, },
     name: { type: String, required: true },
     description: { type: String, maxLength: 600 },
     image: { type: String, maxLength: 255 },
@@ -13,11 +14,13 @@ const Course = new Schema({
     level: { type: String, maxLength: 255 },
     slug: { type: String, slug: 'name' },
 }, {
+    _id: false,
     timestamps: true
 });
 
 // add plugin
 mongoose.plugin(slug)
+// Course.plugin(AutoIncrement)
 Course.plugin(mongooseDelete, {
     deletedAt: true,
     overrideMethods: 'all',
